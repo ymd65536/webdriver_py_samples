411ff8ce78c4c9636a6e2bd1e21c4fbb3770c819
ymd65536 <ymd65536@gmail.com>
2024-01-06 20:38:38 +0900

    modified: get session id

--- 0_SessionId/main.py
+++ 0_SessionId/main.py
@@ -1,6 +1,9 @@
 import requests  # 「pip install requests」などが必要
 import config.const as const
 
+# sleep用
+import os
+
 if __name__ == '__main__':
 
     print("Run WebDriver")
@@ -15,6 +18,10 @@ if __name__ == '__main__':
 
     print("json response {0}".format(res))
     print("{0} {1}".format("sessionId", sessionId))
+
+    for cnt in range(10):
+        print("sleep {0}".format(cnt))
+        os.system('sleep 1')
 
     requests.delete(
         const.WEB_DRIVER_URL + '/' + sessionId,
